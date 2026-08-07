959b7a434fc52206cb36d8a04c4be6d729de1661
Natalia García Cofré <nmt.garcia.c@gmail.com>
2026-08-07 18:35:08 -0400

    perf: cambia fuentes a uso local

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import '@fontsource/dm-sans/400.css'
+import '@fontsource/dm-sans/500.css'
+import '@fontsource/dm-sans/700.css'
+
+import '@fontsource/playfair-display/500.css'
+import '@fontsource/playfair-display/600.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
